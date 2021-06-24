192fdb971b9b06711fc13262517fb0ac47a8fab4
artemats <artem.matseruk@gmail.com>
2021-06-24 15:49:44 +0300

    fix custom select init

--- src/js/select/index.js
+++ src/js/select/index.js
@@ -1,6 +1,8 @@
 import Choices from "choices.js";
 
 const element = document.querySelector('.select');
-const choices = new Choices(element, {
-	searchEnabled: false
-});
+if(!!element) {
+	const choices = new Choices(element, {
+		searchEnabled: false
+	});
+}
