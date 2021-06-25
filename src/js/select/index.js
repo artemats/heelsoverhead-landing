7fc8f846750ac59b3c87ac959efa2240acf9b144
artemats <artem.matseruk@gmail.com>
2021-06-25 16:38:46 +0300

    fix currency select init

--- src/js/select/index.js
+++ src/js/select/index.js
@@ -1,14 +1,17 @@
 import Choices from "choices.js";
 
 const element = document.querySelector('.select');
+const currencySelect = document.querySelector('#currency')
 if(!!element) {
 	const choices = new Choices(element, {
 		searchEnabled: false
 	});
 }
 
-document.querySelector('#currency').addEventListener('change', function () {
-	console.log(this.value);
-	document.cookie = `currency=${this.value}`;
-	document.location.reload();
-});
+if(!!currencySelect) {
+	currencySelect.addEventListener('change', function () {
+		console.log(this.value);
+		document.cookie = `currency=${this.value}`;
+		document.location.reload();
+	});
+}
