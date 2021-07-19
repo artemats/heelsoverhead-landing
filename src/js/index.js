6122bcbe8b9fa97784a2d6d361dc5294bdcbd88a
artemats <artem.matseruk@gmail.com>
2021-07-19 10:36:52 +0300

    fix close button

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import '../sass/index.scss';
 import './subscriptionCheckboxesRules/index';
 import './countdownTimer/index';
 import './tabs/index';
-// import './subscription/index';
+import './subscription/index';
 import './validation/index';
 import './imagesSelect/index';
 import './select/index';
--- src/js/subscription/index.js
+++ src/js/subscription/index.js
@@ -1,34 +1,39 @@
-const form = document.querySelector('#subscription');
-const subscriptionContent = document.querySelector('.subscription-content');
+// const form = document.querySelector('#subscription');
+// const subscriptionContent = document.querySelector('.subscription-content');
+//
+// if(!!form) {
+// 	form.addEventListener('submit', function (e) {
+// 		e.preventDefault();
+//
+// 		// безпосередньо при відправці //
+// 		subscriptionContent.classList.add('sending');
+//
+// 		// теж безпосередньо при відправці, через 100мс //
+// 		setTimeout(function () {
+// 			subscriptionContent.classList.add('start');
+// 		}, 100);
+//
+//
+// 		// при успішній відправці //
+// 		// subscriptionContent.classList.add('success');
+// 		// window.location.href = 'final.html';
+//
+//
+// 		// при помилці //
+// 		setTimeout(function () {
+// 			subscriptionContent.classList.add('error');
+// 		}, 500);
+// 		setTimeout(function () {
+// 			subscriptionContent.classList.remove('sending', 'start', 'error');
+// 			subscriptionContent.classList.add('message');
+// 		}, 1000);
+// 	});
+// }
 
-if(!!form) {
-	const messageYPosition = document.querySelector('.subscription-content-alert').getBoundingClientRect().y;
-
-	form.addEventListener('submit', function (e) {
-		e.preventDefault();
-
-		// безпосередньо при відправці //
-		subscriptionContent.classList.add('sending');
-		window.scrollTo({
-			top: parseInt(messageYPosition),
-			behavior: 'smooth',
-		});
-		// теж безпосередньо при відправці, через 100мс //
-		setTimeout(function () {
-			subscriptionContent.classList.add('start');
-		}, 100);
-
-
-		// при успішній відправці //
-		subscriptionContent.classList.add('success');
-		window.location.href = 'final.html';
-
-
-		// при помилці //
-		subscriptionContent.classList.add('error');
-		setTimeout(function () {
-			subscriptionContent.classList.remove('sending', 'start', 'error');
-		}, 1000);
-
+// close subscription message //
+const closeBtn = document.querySelector('.close-message');
+if(!!closeBtn) {
+	document.querySelector('.close-message').addEventListener('click', function () {
+		document.querySelector('.subscription-content').classList.remove('message');
 	});
 }
