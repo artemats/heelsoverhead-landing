f3add143d016e73ccb61f301cb4a4ade2c956a7c
artemats <artem.matseruk@gmail.com>
2021-07-20 13:28:16 +0300

    fix video playing

--- src/js/toggleVideo/index.js
+++ src/js/toggleVideo/index.js
@@ -1,10 +1,17 @@
+import inView from 'in-view/dist/in-view.min';
 import Plyr from "plyr";
 
-const playerDOM = document.querySelector('#player');
-if(!!playerDOM) {
-	const player = new Plyr(playerDOM, {
+if(!!document.querySelector('#player')) {
+	const player = new Plyr(document.querySelector('#player'), {
 		controls: ['play-large']
 	});
 
-	player.play();
+	inView('#player')
+		.on('enter', function () {
+			player.play();
+		})
+		.on('exit', function () {
+			player.pause();
+		});
+
 }
